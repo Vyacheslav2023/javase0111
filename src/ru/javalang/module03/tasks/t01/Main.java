396d9d442fefb3a67aa7739ac871ccb8f9f4cc2e
Administrator <va.shayakhmetov@gmail.com>
2023-03-28 13:29:14 +0500

    123

--- src/ru/javalang/module03/tasks/t01/Main.java
+++ src/ru/javalang/module03/tasks/t01/Main.java
@@ -1,5 +1,7 @@
 package ru.javalang.module03.tasks.t01;
 
+import java.util.Scanner;
+
 /**
  *
  * Ввести число, и выяснить -
@@ -9,6 +11,11 @@ package ru.javalang.module03.tasks.t01;
 
 public class Main {
     public static void main(String[] args) {
-
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Введите число: ");
+        int x=scan.nextInt();
+        if (x<0) System.out.println("Отрицательное");
+        else if (x==0) System.out.println("Ноль");
+        else System.out.println("Положительное");
     }
 }
