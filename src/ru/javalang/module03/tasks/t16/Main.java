0b5966b6cfbfdc50e2e5849bc53d43036bf63c6a
Administrator <va.shayakhmetov@gmail.com>
2023-03-28 13:59:33 +0500

    123

--- src/ru/javalang/module03/tasks/t16/Main.java
+++ src/ru/javalang/module03/tasks/t16/Main.java
@@ -1,11 +1,21 @@
 package ru.javalang.module03.tasks.t16;
 
+import java.util.Scanner;
+
 /**
  * Вывести на экран горизонтальную линию из звёздочек.
  * Число звездочек указывает пользователь
  */
 public class Main {
     public static void main(String[] args) {
+        System.out.print("Vvedite chislo zvezd: ");
+        int n = new Scanner(System.in).nextInt();
+        zvezdy(n);
+    }
+    public static void zvezdy(int n){
+        for (int i = 0; i < n; i++) {
+            System.out.print("*");
+        }
 
     }
 }
