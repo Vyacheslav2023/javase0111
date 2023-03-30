4de4d49dc48f63934307a9fd4f42dbfc1c7b0f72
Administrator <va.shayakhmetov@gmail.com>
2023-03-30 13:12:22 +0500

    123

--- src/ru/javalang/module10/tasks/t02/Main.java
+++ src/ru/javalang/module10/tasks/t02/Main.java
@@ -1,4 +1,26 @@
 package ru.javalang.module10.tasks.t02;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class Main {
-}
+    public static void main(String[] args) {
+
+    try {
+    FileInputStream fl = new FileInputStream("/home/file.txt");
+
+        while (fl.available()>0){
+            int b = fl.read();
+            System.out.print((char)b + " ");
+        }
+    }
+    catch (FileNotFoundException eee) {
+        System.out.println("Oshibka: "+eee.printStackTrace());
+    }
+    catch (IOException e) {
+        e.printStackTrace();
+        System.out.println("Ok");
+    }
+}}
+
