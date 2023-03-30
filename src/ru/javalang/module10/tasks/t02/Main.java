1f6503cac831064a0eeec086579ab8a67cb6f890
Administrator <va.shayakhmetov@gmail.com>
2023-03-30 13:19:59 +0500

    123

--- src/ru/javalang/module10/tasks/t02/Main.java
+++ src/ru/javalang/module10/tasks/t02/Main.java
@@ -6,21 +6,18 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-
-    try {
-    FileInputStream fl = new FileInputStream("/home/file.txt");
-
-        while (fl.available()>0){
-            int b = fl.read();
-            System.out.print((char)b + " ");
+        try {
+            FileInputStream fl = new FileInputStream("/home/file.txt");
+            while (fl.available() > 0) {
+                int b = fl.read();
+                System.out.print((char) b + " ");
+            }
+        }
+        catch (FileNotFoundException eee) {System.out.println("Oshibka: " + eee);}
+        catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Ok");
         }
     }
-    catch (FileNotFoundException eee) {
-        System.out.println("Oshibka: "+eee.printStackTrace());
-    }
-    catch (IOException e) {
-        e.printStackTrace();
-        System.out.println("Ok");
-    }
-}}
+}
 
