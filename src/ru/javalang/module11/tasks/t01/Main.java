0879b522fa6ab3e664789e85d75b2b654f37b2b0
Administrator <va.shayakhmetov@gmail.com>
2023-03-31 16:59:24 +0500

    123

--- src/ru/javalang/module11/tasks/t01/Main.java
+++ src/ru/javalang/module11/tasks/t01/Main.java
@@ -1,4 +1,21 @@
 package ru.javalang.module11.tasks.t01;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
+    public static void main(String[] args) {
+        InputStreamReader is = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(is);
+        try {
+            System.out.println("Введите первое число:");
+            int a = Integer.parseInt(br.readLine());
+            System.out.println("Введите второе число:");
+            int b = Integer.parseInt(br.readLine());
+            System.out.println("Привет, " + (a+b));
+        } catch (IOException e){
+            System.err.println("Err " + e);
+        }
+    }
 }
--- src/ru/javalang/module11/tasks/t02/Main.java
+++ src/ru/javalang/module11/tasks/t02/Main.java
@@ -1,4 +1,32 @@
 package ru.javalang.module11.tasks.t02;
 
+import java.io.*;
+
 public class Main {
+    public static void main(String[] args) {
+
+        int[] values=new int[];
+        try {
+            BufferedReader in = new BufferedReader(new FileReader("in.txt")); //InputStreamReader(System.in));
+            int count = 0;
+            for (String line = in.readLine(); line != null; line = in.readLine()) {
+                values[count] = Integer.parseInt(line);
+                count++;
+            }
+            in.close();
+        } catch (IOException e) {
+            System.out.println("Some error occurred!");
+        }
+
+        try {
+            DataOutputStream out = new DataOutputStream(new FileOutputStream("out.bin"));
+            for (int i = 0; i < values.length; i++) {
+                out.writeInt(values[i]);
+            }
+            out.close();
+        } catch (
+                IOException e) {
+            System.out.println("Some error occurred!");
+        }
+    }
 }
